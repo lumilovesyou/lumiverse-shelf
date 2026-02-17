87173b641d234bfd4bd2cff94fef140d35f5f588
mewo <177341604+lumilovesyou@users.noreply.github.com>
2026-02-17 14:03:09 -0600

    Good enough
    Not super happy with it but I'm sure I'll revisit in the future and redo it all again!

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const shelfContainingDiv = document.createElement("div");
-    shelfContainingDiv.id = "shelfContainingDiv"
-    shelfContainingDiv.innerHTML = `
-        <img src="./assets/images/pixelated-wooden-shelf.png">
-        <div id="shelvesHolder">
-            <div id="shelfOne">
-                <img src="./assets/shelves/shelf-1/pixelated-cartridges.png" data-link="https://lumilovesyou.itch.io/">
-            </div>
-            <div id="shelfTwo"></div>
-            <div id="ShelfThree"></div>
-            <div id="shelfFour"></div>
-        </div>
-    `;
-
-    document.getElementsByTagName("center")[0].replaceWith(shelfContainingDiv);
-
-    Array.from(document.getElementsByTagName("img")).forEach((item) => {
-        if (item.dataset.link) {
-            item.addEventListener("click", (e) => {
-                window.open(e.target.dataset.link, '_blank').focus();
-            });
+    const params = new URLSearchParams(document.location.search); if (params.get("unfancy")) {
+        for (let i = 0; i < document.styleSheets.length; i++) {
+            void(document.styleSheets.item(i).disabled=true);
         }
-    });
+    } else {
+        const shelfContainingDiv = document.createElement("div");
+        shelfContainingDiv.id = "shelfContainingDiv"
+        shelfContainingDiv.innerHTML = `
+            <img src="./assets/fancy/wooden-shelf.webp">
+            <div id="shelvesHolder">
+                <div id="shelfOne"></div>
+                <div id="shelfTwo">
+                    <span data-size="small"></span><img src="./assets/fancy/pixelated-cassettes.webp" data-link="https://www.youtube.com/playlist?list=PLRBlqcQ05_RTX0GvFCIH8PtgzqMOMa5ie"><span data-size="large"></span><img src="./assets/fancy/pixelated-plushies.webp" style="--rotate:-7deg" data-link="https://modrinth.com/user/LumiLovesYou">
+                </div>
+                <div id="shelfThree">
+                    <span data-size="large"></span><img src="./assets/fancy/pixelated-cartridges.webp" data-link="https://lumilovesyou.itch.io/"><span data-size="large"></span><span data-size="large"></span>
+                </div>
+                <div id="shelfFour">
+                    <img src="./assets/fancy/pixelated-crumpled-ucrl.webp" style="--rotate:-7deg" data-link="https://github.com/lumilovesyou/Unofficial-Cosmic-Reach-Launcher"><span data-size="large"></span><span data-size="medium"></span>
+                </div>
+            </div>
+        `;
+
+        document.getElementsByTagName("center")[0].replaceWith(shelfContainingDiv);
+
+        Array.from(document.getElementsByTagName("img")).forEach((item) => {
+            if (item.dataset.link) {
+                item.addEventListener("click", (e) => {
+                    window.open(e.target.dataset.link, '_blank').focus();
+                });
+            }
+        });
+    }
 });
